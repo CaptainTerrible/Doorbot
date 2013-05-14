0c6b1cf6f6464e3f098cf12ea5f2a9358164745e
Tim Reynolds <tim@christwithfries.net>
2013-05-14 21:34:00 +0100

    Barebones ethernet code. Multicast harder.

--- listeners/examples/doorbot_listener_example/doorbot_listener_example.ino
+++ listeners/examples/doorbot_listener_example/doorbot_listener_example.ino
@@ -1,1 +1,60 @@
+/*
+London Hackspace Membership Counter
+ 
+ LED Segment shifting, displaydata, etc are from Matt Little (info@re-innovation.co.uk / www.re-innovation.co.uk)
+ Remainder butched by T Reynolds ( tim@christwithfries.net / www.appliedpraxis.com )
+ 
+ */
+
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <util.h>
+#include <SPI.h>
+
+byte mac[] = {  
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+unsigned int BroadcastPort = 50000;
+
+EthernetUDP UDP;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Example doorbot listener starting up.");
+
+  Serial.print("Attempting to get IP via DHCP...");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("failed.");
+    Serial.println("Unable to configure Ethernet using DHCP, halting.");
+    for(;;)
+      ;
+  } 
+  else {
+    Serial.println("done.");
+    Serial.print("IP address: ");
+    for (byte thisByte = 0; thisByte < 4; thisByte++) {
+      Serial.print(Ethernet.localIP()[thisByte], DEC);
+      Serial.print("."); 
+    }
+  }
+
+  Serial.println();
+  
+  /*
+    The standard Arduino ethernet library does not support multicast. Modified version does.
+    http://tkkrlab.nl/wiki/Arduino_UDP_multicast
+  */
+  
+}
+
+void loop()
+{
+
+}
+
 
