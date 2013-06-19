9e7d818e6f2d02b4716a5f9d13450149aad50f58
Tim Reynolds <tim@christwithfries.net>
2013-06-19 23:52:48 +0100

    Gutted all packet processing. Still doesn't work. Buffer is reused, can't work out how to clear it reliably.

--- listeners/examples/doorbot_listener_example/doorbot_listener_example.ino
+++ listeners/examples/doorbot_listener_example/doorbot_listener_example.ino
@@ -34,12 +34,12 @@ void setup()
 {
   //Enable onboard LED
   pinMode(13, OUTPUT);
-  
+
   //Setup serial monitor
   Serial.begin(9600);
-  
+
   Serial.println("Example doorbot listener starting up.");
-  
+
   Serial.print("Attempting to get IP via DHCP...");
   if (Ethernet.begin(mac) == 0) {
     Serial.println("failed.");
@@ -57,12 +57,12 @@ void setup()
       Serial.print("."); 
     }
   }
-  
+
   Serial.println();
   Serial.print("Starting listener on port ");
   Serial.print(ListenPort);
   Serial.println();
-  
+
   //We have an IP, start listening.
   DoorBotListener.begin(ListenPort);
 
@@ -72,97 +72,16 @@ void loop()
 {
   //Check if we have data waiting
   int packetSize = DoorBotListener.parsePacket();
-  
+
   if(packetSize)
-  {
-    
-    String msgPacket = "";
-    String msgDoor = "";
-    int msgFirstTab = 0;
-    int msgSecondTab = 0;    
-    String msgEvent = "";
-    String msgSerial = "";
-    String msgName = "";
- 
-    //Flash LED twice, with 0.5s delay
-    FlashLed(2,500);   
-    
-    //Save the IP and port of the remote machine that sent the packet.
-    int msgRemotePort = DoorBotListener.remotePort();
-    IPAddress msgRemoteIP = DoorBotListener.remoteIP();
-    
-    //Zero packetBuffer    
-    int i = 0;
-    while (i < packetSize) {      
-      PacketBuffer[i] = 0;
-      i++;
-    }
-    
+  { 
     //Read the waiting data
     DoorBotListener.read(PacketBuffer,MaxPacketSize);
 
-    //Copy packetBuffer into a string object  
-    msgPacket.concat(PacketBuffer);
-
-    //Work out what door we're dealing with
-    switch (ListenPort) {
-    case 50000:
-      msgDoor = "back";
-      break;
-    case 50002:
-      msgDoor = "front";
-      break;
-    default:
-      msgDoor = "unknown"; //Beware of those entering from this door. 
-    }   
-
-    //Split up msgPacket - this is hacky and doesn't work. **FIXME**    
-
-    //Find the first tab character, after the event type
-    msgFirstTab = msgPacket.indexOf('\n');
-    //Find the second tab character, after the serial but before the name
-    msgSecondTab = msgPacket.indexOf('\n',msgPacket.length());
-    //First four characters are the event type 
-    msgEvent = msgPacket.substring(0,4);
-    //Characters between the first and second tabs are the serial
-    msgSerial = msgPacket.substring(msgFirstTab+1,msgSecondTab);
-    //Characters after the second tab are the name
-    msgName = msgPacket.substring(msgSecondTab+1);
-
-    //Dump the split parts of the event to serial
-    Serial.print("Doorbot event from ");
-    Serial.print(msgRemoteIP);
-    Serial.print(":");
-    Serial.print(msgRemotePort);
-    Serial.println(" {");
-    Serial.print("Door: ");
-    Serial.println(msgDoor);
-    Serial.print("Event type: ");
-    Serial.println(msgEvent);
-    Serial.print("Serial: ");
-    Serial.println(msgSerial);    
-    Serial.print("Name: ");
-    Serial.println(msgName);        
-    Serial.println("}");
-
-    //Dump the raw packet received to aid in debugging
-    Serial.println("==Raw Dump Start=="); 
+    //Dump the raw packet
     Serial.println(PacketBuffer);
-    Serial.println("==Raw Dump End==");
+
+    //**FIXME** PacketBuffer is reused and full of previous events.
   }
 }
 
-void FlashLed(int flashnum, int flashdelay)
-{
-  int i = 0;
-  
-  while (i < flashnum)
-  {  
-  digitalWrite(13, HIGH);
-  delay(flashdelay);
-  digitalWrite(13, LOW);
-  delay(flashdelay);
-  i++;
-  Serial.println("FLASH!");
-  }
-}
